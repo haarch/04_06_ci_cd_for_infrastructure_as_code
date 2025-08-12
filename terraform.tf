e2de02e98ccd6675b45ef6644925de8ed3fbd08e
Chris Haars <chris.haars@pearson.com>
2025-08-12 08:51:25 -0500

    initial changes

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "~> 1.4"
+
+  backend "s3" {
+    key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
+    bucket = "haars-deleteme-tf-state-125616717413"  # the bucket
+    region = "us-east-1"                             # the region
+  }
+}
